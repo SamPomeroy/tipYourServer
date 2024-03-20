513d5717c1db6b700ed8cd18e24b58875011721b
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-03-19 19:43:03 -0500

    complete

--- server.js
+++ server.js
@@ -1,34 +1,71 @@
 const http = require('http')
 const fs = require('fs')
-
 http
     .createServer(function(request, response){
         console.log(request)
-        if(request.url === "/create-directory"){
-            fs.mkdir('content', function(error,){
-                console.log(error)
-                if(error){
-                    response.end()
-                } else {
-                    response.end('content folder created')
-                }
+        if(request.url === "/create-directory" && request.method === "GET"){
+            let body = ''
+            request.on('data',function(data){
+                body += data.toString()
             })
-        }else if (request.url === "/create-text"){
-                fs.writeFile('randomText.txt', function(error, data){
+            request.on("end",function(){
+                let parsedBody = JSON.parse(body)
+                fs.mkdir(parsedBody.folderName,function(error){
                     if(error){
-                        response.end()
-                    } else{
-                        
-                        response.end('randomtext.txt created')
+                        response.end(error)
+                    } else {
+                        response.end("content folder created")
                     }
                 })
-            
-            }else{
-                return response.end()
-        } 
+            })
+        } else if(request.url === "/create-text" && request.method === "GET"){
+            let body = ''
+            request.on("data",function (data){
+                body += data.toString()
+            })
+            request.on('end',function(){
+                let parsedBody = JSON.parse(body)
+                fs.writeFile(parsedBody.fileName,parsedBody.randomText,function(error){
+                    if(error){
+                        response.end(error)
+                    } else {
+                        response.end(parsedBody.randomText)
+                    }
+                })
+            })
+        } else if(request.url === "/new-folder-and-file" && request.method === "GET"){
+            let body = ''
+            request.on("data",function (data){
+                body += data.toString()
+            })
+            request.on("end",function(){
+                let parsedBody = JSON.parse(body)
+                fs.mkdir(parsedBody.folderName, function(error){
+                    if(error){
+                        response.end(error)
+                    } else {
+                        fs.writeFile(parsedBody.secondFileName,parsedBody.randomText,function(error){
+                            if(error){
+                                response.end(error)
+                            } else {
+                                response.end(parsedBody.randomText)
+                                setTimeout(function(){
+                                    fs.rmdir(parsedBody.folderName,{recursive:true},function(error){
+                                        if(error){
+                                            response.end(error)
+                                        } else {
+                                            response.end("File is Deleted")
+                                        }
+                                    })
+                                },7000)
+                            }
+                        })
+                    }
+                })
+            })
+        }
     })
     .listen(3001,function(){
         // console.log("HelloWorld")
         console.log("Server Started!!!")
     })
-       
